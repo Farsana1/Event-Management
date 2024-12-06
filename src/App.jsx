90dd9fcf3e03c5a54e7c56f395835e1a88b27625
Farsana1 <pifarsana@gmail.com>
2024-12-06 07:42:32 +0530

    basic-project-setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  // js code
+
+  return (
+    <>
+      {/* jsx content */}
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/HouseWarming.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function HouseWarming() {
+  return (
+    <div>HouseWarming</div>
+  )
+}
+
+export default HouseWarming
