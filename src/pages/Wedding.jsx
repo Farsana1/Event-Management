0e1911c794e350511b6fbe8775bac8dc8f35d5d5
Farsana1 <pifarsana@gmail.com>
2024-12-07 22:39:12 +0530

    next

--- src/pages/Wedding.jsx
+++ src/pages/Wedding.jsx
@@ -4,15 +4,31 @@ import './Wedding.css'
 function Wedding() {
   return (
     <>
+    {/* landing */}
     <div className='container-fluid w-100' id='main' >
     <div id='bg' >
-      <div className='text-light flex-column d-flex justify-content-center align-items-center w-100' style={{height:'100vh'}}>
-        <h2><strong>Crafting Your Perfect Day with Elegance and Seamless Perfection</strong></h2>
-        <p>We create unforgettable wedding experiences by blending your unique vision with meticulous planning and flawless execution. From the smallest details to the grandest arrangements, we ensure every moment of your special day is magical, stress-free, and a true reflection of your love story.</p>
+      <div className='text-light flex-column d-flex justify-content-center align-items-center mx-3' style={{height:'100vh'}}>
+        <h2 id='tag'><strong>Crafting Your Perfect Day with <br /> Elegance and Seamless Perfection</strong></h2>
+        <p>We create unforgettable wedding experiences by blending your unique vision with <br /> meticulous planning and flawless execution.
+          From the smallest details to the grandest arrangements, <br /> we ensure every moment of your special day is magical, stress-free, and a true reflection of your love story.</p>
+
+          <div className='d-flex'>  
+            <button className='btn btn-success text-light shadow'>Get Started</button>
+          </div>
+
       </div>
+     
+    </div>
+     
     </div>
 
-    </div>
+    {/*  */}
+
+
+    {/* Venues */}
+
+    {/* Photography */}
+
     </>
   )
 }
