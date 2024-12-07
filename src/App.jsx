4ed5a210790db7537166e65cc2fb9fe20de0042e
Farsana1 <pifarsana@gmail.com>
2024-12-07 16:24:55 +0530

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,10 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Home from './pages/Home'
+import Wedding from './pages/Wedding'
+import Birthday from './pages/Birthday'
 
 function App() {
   // js code
@@ -6,6 +12,14 @@ function App() {
   return (
     <>
       {/* jsx content */}
+      <Header/>
+    <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/wedding' element={<Wedding/>}/>
+        <Route path='/birthday' element={<Birthday/>}/>
+    </Routes>
+    <Footer/>
+      
     </>
   )
 }
--- src/pages/Wedding.jsx
+++ src/pages/Wedding.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import './Wedding.css'
 
 function Wedding() {
   return (
-    <div>Wedding</div>
+    <>
+    <div className='container-fluid w-100' id='main' >
+    <div id='bg' >
+      <div className='text-light flex-column d-flex justify-content-center align-items-center w-100' style={{height:'100vh'}}>
+        <h2><strong>Crafting Your Perfect Day with Elegance and Seamless Perfection</strong></h2>
+        <p>We create unforgettable wedding experiences by blending your unique vision with meticulous planning and flawless execution. From the smallest details to the grandest arrangements, we ensure every moment of your special day is magical, stress-free, and a true reflection of your love story.</p>
+      </div>
+    </div>
+
+
+    </div>
+    </>
   )
 }
 
