2956eccfb5aac8655d4e3260e7900a16cd372290
Farsana1 <pifarsana@gmail.com>
2025-01-18 21:59:22 +0530

    cleaning

--- src/pages/LoginRegister.jsx
+++ src/pages/LoginRegister.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import './Loginregister.css';
+import './LoginRegister.css';
 import { serverUrl } from '../server';
 import { useNavigate } from 'react-router-dom';
 import { ToastContainer, toast } from 'react-toastify';
